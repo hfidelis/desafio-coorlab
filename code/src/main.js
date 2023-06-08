903b77561abdbcbe887716a389c079be4a236d8c
Heitor Fidelis <heitorc88@gmail.com>
2023-06-08 17:28:43 -0300

    feat: import BootstrapVueIcons

--- code/src/main.js
+++ code/src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin, BootstrapVueIcons } from 'bootstrap-vue'
 import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
